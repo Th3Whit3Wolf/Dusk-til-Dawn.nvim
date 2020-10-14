dbf6974c2cddea88a01a7d94aa46d319e4983cac
David Karrick <the.white.wolf.is.1337@gmail.com>
2020-10-14 15:10:55 +0100

    Format

--- lua/Dusk-til-Dawn.lua
+++ lua/Dusk-til-Dawn.lua
@@ -3,23 +3,23 @@ local package = package
 
 found = false
 local paths = {}
-for match in (package.path..";"):gmatch("(.-);") do
+for match in (package.path .. ";"):gmatch("(.-);") do
     table.insert(paths, match);
 end
-for k,_ in pairs(paths) do
+for k, _ in pairs(paths) do
     local dirs = {}
-    for match in (paths[k].."/"):gmatch("(.-)/") do
+    for match in (paths[k] .. "/"):gmatch("(.-)/") do
         table.insert(dirs, match);
     end
-    for i=1, #dirs do
-        if found then break end
+    for i = 1, #dirs do
+        if found then
+            break
+        end
         if dirs[#dirs + 1 - i] == 'Dusk-til-Dawn.nvim' then
-            local str = table.concat(dirs , '/' , 1 , #dirs + 1 - i )
+            local str = table.concat(dirs, '/', 1, #dirs + 1 - i)
             vim.cmd("luafile " .. str .. "/lua/lib/Dusk-til-Dawn.lua")
             vim.cmd("command! ToggleColorscheme lua changeColors()")
             found = true
         end
     end
 end
-
-
